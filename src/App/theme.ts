60c8cead7105eab735f7ab003e8f6a4f8240aac3
anikethsaha <anik220798@gmail.com>
2019-10-29 11:33:15 +0530

    chore: dark mode setup 3

---
+++ src/App/theme.ts
@@ -0,0 +1,18 @@
+import colors from '../utils/styles/colors';
+export const darkTheme = {
+  backGround: '#333',
+  dimBackGround: '#212121',
+  hover: '#333',
+  iconLightColor: 'rgba(248, 248, 248, 0.54)',
+  textColor: '#ebebeb',
+  footer: '#333',
+};
+
+export const defaultTheme = {
+  backGround: colors.primary,
+  dimBackGround: 'white',
+  hover: '#f3f4f240',
+  iconLightColor: 'rgba(0, 0, 0, 0.54)',
+  textColor: '#586069',
+  footer: colors.snow,
+};
